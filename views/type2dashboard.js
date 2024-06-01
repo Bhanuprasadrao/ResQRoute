7bfb7a53604e4d6609d8d430ea84e769213eccbe
Kolamaddi Bhanu Prasad Rao <bhanuprasad@greenitive.com>
2024-06-01 12:17:00 +0530

    Updated WebSocket URL

--- views/type2dashboard.js
+++ views/type2dashboard.js
@@ -18,14 +18,12 @@ document.getElementById('stopRide').addEventListener('click', () => {
 function sendLocation(position) {
     const { latitude, longitude } = position.coords;
 
-    // Send latitude and longitude to server
     const data = {
-        vehicleType: 'public', // Identify as public user
+        vehicleType: 'public',
         latitude,
         longitude
     };
 
-    // Make an AJAX request to the server to update location
     fetch('/update-location', {
         method: 'POST',
         headers: {
@@ -45,12 +43,11 @@ function sendLocation(position) {
 }
 
 function handleLocationError(error) {
-    // Handle geolocation errors
     console.error('Error getting location:', error.message);
 }
 
 // Event listener to receive alert messages via WebSocket
-const socket = new WebSocket('wss://res-qr-oute2-0.vercel.app');
+const socket = new WebSocket('wss://res-qr-oute2-0-ir6s2dqby-bhanu-prasad-raos-projects.vercel.app');
 
 socket.onmessage = (event) => {
     const alertMessage = event.data;
@@ -70,5 +67,5 @@ function displayAlert(message) {
 
     setTimeout(() => {
         alertElement.remove();
-    }, 5000); // Remove alert after 5 seconds
+    }, 5000);
 }
